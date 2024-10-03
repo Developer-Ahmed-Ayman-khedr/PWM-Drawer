6a7c710b481ed7f18359a01e6c2d1e44d239ece1
Ahmed Ayman Khedr <94747792+Developer-Ahmed-Ayman-khedr@users.noreply.github.com>
2024-10-04 01:01:57 +0300

    Update.

--- MainCode/Debug/HAL/LCD/LCD_Program.d
+++ MainCode/Debug/HAL/LCD/LCD_Program.d
@@ -1,15 +1,15 @@
 HAL/LCD/LCD_Program.o HAL/LCD/LCD_Program.o: ../HAL/LCD/LCD_Program.c \
-  D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h \
-  D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h \
-  D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h \
+  S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h \
+  S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h \
+  S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h \
   ../HAL/LCD/LCD_Interface.h ../HAL/LCD/LCD_private.h \
   ../HAL/LCD/LCD_Config.h
 
-D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h:
+S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h:
 
-D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h:
+S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h:
 
-D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h:
+S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h:
 
 ../HAL/LCD/LCD_Interface.h:
 
--- MainCode/Debug/MCAL/TMR0/TMR0_Program.d
+++ MainCode/Debug/MCAL/TMR0/TMR0_Program.d
@@ -1,16 +1,16 @@
 MCAL/TMR0/TMR0_Program.o MCAL/TMR0/TMR0_Program.o:  \
  ../MCAL/TMR0/TMR0_Program.c \
-  D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h \
-  D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h \
-  D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h \
+  S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h \
+  S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h \
+  S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h \
   ../MCAL/TMR0/TMR0_Interface.h ../MCAL/TMR0/TMR0_Private.h \
   ../MCAL/TMR0/TMR0_Config.h
 
-D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h:
+S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/STD_TYPES.h:
 
-D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h:
+S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\LIB/BIT_MATH.h:
 
-D:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h:
+S:\Courses\Programming\Embedded_Systems_Course\MyProjects\PWM-Drawer\MainCode\MCAL\DIO/DIO_Interface.h:
 
 ../MCAL/TMR0/TMR0_Interface.h:
 
